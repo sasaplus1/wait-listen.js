4aab6f2f00ababbb879f23542f1f66fd4fd6f04e
sasaplus1 <sasaplus1@gmail.com>
2023-03-13 10:03:15 +0900

    convert string to number

--- bin.js
+++ bin.js
@@ -9,14 +9,18 @@ const { executeCommand, waitListen } = require('./index');
 
 const log = debug('wait-listen');
 
+// NOTE: Why convert string to number with Number(), see below:
+// JavaScript で parseInt / parseFloat を使わない方が良い理由
+// http://nmi.jp/2022-02-03-dont-use-parseInt
+
 program
   .name(name)
   .description('wait for server to listen')
   .option('-c, --command <command>', 'execute command', '')
   .option('-d, --debug', `show command's stdout and stderr`, false)
-  .option('-i, --interval <ms>', 'request interval', 500)
-  .option('-s, --status <code>', 'target status code', 200)
-  .option('-t, --timeout <ms>', 'timeout', 1000 * 60 * 3)
+  .option('-i, --interval <ms>', 'request interval', Number, 500)
+  .option('-s, --status <code>', 'target status code', Number, 200)
+  .option('-t, --timeout <ms>', 'timeout', Number, 1000 * 60 * 3)
   .version(version, '-v, --version')
   .action(onAction);
 
